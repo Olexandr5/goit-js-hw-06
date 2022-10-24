bc2a551fa7355dd46ebc535af2d68ad5b8362d09
Olexandr Sherbakov <107261823+Olexandr5@users.noreply.github.com>
2022-10-24 22:01:08 +0300

    0.9
    0.9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const emailElm = document.querySelector(".login-form");
+emailElm.addEventListener("submit", emailElmSubmite);
+function emailElmSubmite(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const userAcount = { email: email.value, password: password.value };
+  console.log("userAcount", userAcount);
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeButon = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorChange = document.querySelector(".color");
+
+changeButon.addEventListener("click", changeColor);
+function changeColor() {
+  let color = getRandomHexColor();
+  body.style.background = color;
+  colorChange.textContent = color;
+}
