36daf2fff549c93c2cece52803a90d3d26a027a7
Olexandr Sherbakov <107261823+Olexandr5@users.noreply.github.com>
2022-10-26 08:10:22 +0300

    1
    1

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const fontSize = document.querySelector("#font-size-control");
 const valueText = document.querySelector("#text");
 
-fontSize.addEventListener("change", sizeText);
+fontSize.addEventListener("input", sizeText);
 function sizeText(event) {
   valueText.style.fontSize = `${event.target.value}px`;
 }
