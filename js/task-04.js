c2f1a25694a2b60dfe6f265024d11bb1c886d536
Olexandr Sherbakov <107261823+Olexandr5@users.noreply.github.com>
2022-12-15 07:35:25 +0200

    1.2
    1.2

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,29 @@
-let countValues = 0;
-const valueElm = document.querySelector("#value");
-const plusElm = document.querySelector('button[data-action="increment"]');
-const minusElm = document.querySelector('button[data-action="decrement"]');
-
-plusElm.addEventListener("click", () => {
-  countValues++;
-  valueElm.textContent = countValues;
-});
-minusElm.addEventListener("click", () => {
-  countValues--;
-  valueElm.textContent = countValues;
-});
+class Counter {
+  constructor(selector, step, initialValue) {
+    this.counterContainer = document.querySelector(selector);
+    this.incrementBtn = this.counterContainer.querySelector("[data-action='increment']");
+    this.dicrementBtn = this.counterContainer.querySelector("[data-action='decrement']");
+    this.spanRef = this.counterContainer.querySelector('#value');
+    this.step = step;
+    this.initialValue = initialValue;
+    this.addListeners();
+  }
+  addListeners = () => {
+    this.spanRef.textContent = this.initialValue;
+    this.incrementBtn.textContent = `+ ${this.step}`;
+    this.dicrementBtn.textContent = `- ${this.step}`;
+    this.incrementBtn.addEventListener('click', this.hendelIncriment);
+    this.dicrementBtn.addEventListener('click', this.hendelDecrement);
+  };
+  hendelIncriment = () => {
+    this.initialValue += this.step;
+    this.spanRef.textContent = this.initialValue;
+  };
+  hendelDecrement = () => {
+    this.initialValue -= this.step;
+    this.spanRef.textContent = this.initialValue;
+  };
+}
+const counter1 = new Counter('#counter', 5, 0);
+// counter1.addListeners();
+const counter2 = new Counter('#counter2', 10, 100);
--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,19 @@
-const nameInput = document.querySelector("#name-input");
-nameInput.addEventListener("input", onInput);
-const nameOutput = document.querySelector("#name-output");
+// const nameInput = document.querySelector("#name-input");
+// nameInput.addEventListener("input", onInput);
+// const nameOutput = document.querySelector("#name-output");
 
-function onInput(event) {
-  nameOutput.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === "") {
-    nameOutput.textContent = "Anonymous";
-  }
+// function onInput(event) {
+//   nameOutput.textContent = event.currentTarget.value;
+//   if (event.currentTarget.value === "") {
+//     nameOutput.textContent = "Anonymous";
+//   }
+// }
+
+const refs = {
+  input: document.querySelector('#name-input'),
+  span: document.querySelector('#name-input'),
+};
+refs.input.addEventListener('input', onChange);
+function onChange(event) {
+  refs.span.textContent = event.target.value;
 }
